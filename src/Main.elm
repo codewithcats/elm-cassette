b8ac074c1b8d9ff186780cf612240542551675b6
Tanawat Tassana <amp.tanawat@gmail.com>
2018-12-17 02:06:31 +0700

    Make a request on init

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,9 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 import Browser
 import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (src)
+import Http
+import Json.Decode exposing (Decoder, string, succeed)
+import Json.Decode.Pipeline exposing (required)
 
 
 
@@ -10,7 +13,13 @@ import Html.Attributes exposing (src)
 
 
 type alias Flags =
-    { spotifyClientId : String, spotifySecret : String }
+    { spotifyClientId : String
+    , spotifySecret : String
+    }
+
+
+type alias SpotifyAccess =
+    { access_token : String }
 
 
 type alias Model =
@@ -24,8 +33,18 @@ init flags =
     ( { spotifyClientId = flags.spotifyClientId
       , spotifySecret = flags.spotifySecret
       }
-    , Cmd.none
+    , getSpotifyAccess flags.spotifyClientId
+        flags.spotifySecret
     )
+
+
+
+---- JSON ----
+
+
+spotifyAccessDecoder =
+    succeed SpotifyAccess
+        |> required "access_token" string
 
 
 
@@ -33,12 +52,30 @@ init flags =
 
 
 type Msg
-    = NoOp
+    = UpdateSpotifyAccess (Result Http.Error SpotifyAccess)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     ( model, Cmd.none )
+
+
+getSpotifyAccess : String -> String -> Cmd Msg
+getSpotifyAccess clientId secret =
+    Http.request
+        { method = "POST"
+        , url = "https://accounts.spotify.com/api/token"
+        , body =
+            Http.stringBody "application/x-www-form-urlencoded"
+                "grant_type=client_credentials"
+        , headers =
+            [ Http.header "Authorization"
+                ("Basic " ++ clientId ++ ":" ++ secret)
+            ]
+        , expect = Http.expectJson UpdateSpotifyAccess spotifyAccessDecoder
+        , timeout = Nothing
+        , tracker = Nothing
+        }
 
 
 
